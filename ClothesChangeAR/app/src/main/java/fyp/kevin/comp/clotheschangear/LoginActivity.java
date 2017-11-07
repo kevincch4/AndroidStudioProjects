40473285844913b49a094cb044ef1ac124403a97
kevincch4 <chengchunhei4@gmail.com>
2017-11-07 17:48:07 +0800

    new

---
+++ ClothesChangeAR/app/src/main/java/fyp/kevin/comp/clotheschangear/LoginActivity.java
@@ -0,0 +1,15 @@
+package fyp.kevin.comp.clotheschangear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//import com.Facebook.FacebookSdk;
+//import com.facebook.appevents.AppEventsLogger
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- FinalYearProject/app/src/main/java/fyp/kevin/comp/finalyearproject/MainActivity.java
+++ FinalYearProject/app/src/main/java/fyp/kevin/comp/finalyearproject/MainActivity.java
@@ -1,134 +1,97 @@
 package fyp.kevin.comp.finalyearproject;
 
+
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
-    private static int RC_SIGN_IN = 0;
-    private static String TAG = "MAIN_ACTIVITY";
-    private GoogleApiClient mGoogleApiClient;
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class MainActivity extends AppCompatActivity {
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Log.d("AUTH", "user logged in: " + user.getEmail());
-                } else {
-                    Log.d("AUTH", "user logged out ");
-                }
+            public void onSuccess(LoginResult loginResult) {
+                String userid = loginResult.getAccessToken().getUserId();
+
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        displayUserInfo(object);
+                    }
+                });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "first_name, last_name, email, id");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
             }
-        };
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
+            @Override
+            public void onCancel() {
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
+            }
 
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
-        findViewById(R.id.sign_out_button).setOnClickListener(this);
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
+    public void displayUserInfo(JSONObject object){
+        String first_name, last_name, email, id;
+        first_name = "";
+        last_name="";
+        email = "";
+        id = "";
+        try {
+            first_name = object.getString("first_name");
+            last_name = object.getString("last_name");
+            email = object.getString("email");
+            id = object.getString("id");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        TextView view_name, view_email,view_id;
+        view_name = (TextView)findViewById(R.id.view_name);
+        view_email = (TextView)findViewById(R.id.view_email);
+        view_id = (TextView)findViewById(R.id.view_id);
+
+        view_name.setText(first_name+" "+ last_name);
+        view_email.setText(email);
+        view_id.setText("ID: "+id);
+
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if(mAuthListener!= null)
-            mAuth.removeAuthStateListener(mAuthListener);
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                GoogleSignInAccount account = result.getSignInAccount();
-                firebaseAuthWithGoogle(account);
-            } else {
-                Log.d(TAG, "Google log in failed.");
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d("AUTH", "signInWithCredential:oncomplete: " + task.isSuccessful());
-                    }
-                });
-    }
-
-    @Override
-    public void onClick(View view) {
-//        switch (view.getId()) {
-//            case R.id.sign_in_button:
-//                signIn();
-//                break;
-//            case R.id.sign_out_button:
-//                signOut();
-//                break;
-//        }
-
-        if (view.getId() == R.id.sign_in_button) {
-            signIn();
-        } else if (view.getId() == R.id.sign_out_button) {
-            signOut();
-        } else {
-            Log.d("Recognise button", "FAILED");
-        }
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d(TAG, "Connection Failed");
-    }
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    private void signOut() {
-        FirebaseAuth.getInstance().signOut();
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
